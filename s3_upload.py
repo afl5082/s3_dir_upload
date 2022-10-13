cd66cb7529bc29d9b03e53dd262b306e0fdf7d01
Adam <41493452+afl5082@users.noreply.github.com>
2022-10-12 21:30:48 -0400

    Update s3_upload.py

--- s3_upload.py
+++ s3_upload.py
@@ -13,18 +13,32 @@ def aws_conn():
 def uploadDirectory(repo_name,bucketname):
 
     s3 = aws_conn()
-    ignore_files_dir = ['__pycache__','upload_s3.py','.prefectignore']
+    ignore_files_dir = [
+        '__pycache__',
+        's3_upload.py',
+        '.prefectignore',
+        '.git'
+        's3_upload'
+        '.github'
+        '.gitignore'
+        ]
 
     for root,dirs,files in os.walk('.'):
+       
         for file in files:
-            if file in ignore_files_dir or 'main.cpython' in file:
+            if file in ignore_files_dir:
                 continue
 
             path = os.path.join(root,file)
-            file_path_to_upload = path.replace('./','')
-            file_path_to_upload = '{}{}{}'.format(repo_name,'/',file_path_to_upload)
-            print(path,bucketname,file_path_to_upload)
-            s3.upload_file(os.path.join(root,file),bucketname,file_path_to_upload)
+            root_dir = path.split('/')[1]
+            for dir in root_dir:
+                if dir in ignore_files_dir:
+                    continue
+                else:
+                    file_path_to_upload = path.replace('./','')
+                    file_path_to_upload = '{}{}{}'.format(repo_name,'/',file_path_to_upload)
+                    print(path,bucketname,file_path_to_upload)
+                    s3.upload_file(path,bucketname,file_path_to_upload)
 
 def main():
     repo_name = os.environ.get("REPO_NAME")
